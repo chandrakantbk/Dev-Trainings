aecb8fac61aa4512f3d27981053b4a9fa3079c9b
chandrakantbk <chandrakantbk.cs@gmail.com>
2019-12-26 10:56:23 +0530

    understanding while loop and solve a assignment

---
+++ JavaScript/Day-67-Looping/looping.js
@@ -0,0 +1,65 @@
+/**
+* @Auther: Chandrakant
+* @Description: Looping expamle
+* @Date: 25-Dec-2019
+*
+* @Copyright: MIT Licence
+*
+*/
+
+var index = 0; 
+
+while( index < 100 ){
+	
+	index++;
+	
+	if( index % 2 == 0 ){
+		continue;
+	}
+	
+	if(index == 21 ){
+		break;
+	}
+	
+	console.log( index )
+	
+}
+
+
+
+
+
+
+
+
+/* Logincal Operators */
+
+/* 
+a= "10"  b = 10
+
+<   a < b  yes 
+>   a > b  no
+==  a == b  no
+  
+!=  a !=b  yes
+
+!   !a    false
+
+	a == b  yes 
+	
+===  a === b NO
+	 
+!== 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
