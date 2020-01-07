c305d324f364566f903ef041eaf57b9c4c1d6e5e
chandrakantbk <chandrakantbk.cs@gmail.com>
2020-01-07 22:00:25 +0530

    Immutable data structure

---
+++ JavaScript/Day-74-IO/io.js
@@ -0,0 +1,17 @@
+
+function calc(){
+	var num1; //undefined
+	var num2; //undefined
+	var answer; //undefined
+	
+	num1 = document.getElementById("num-1").value; //"10"
+	
+	num2 = document.getElementById("num-2").value //"20"
+	
+	//answer = "10" + "20" ==>> 1020 //typeof : string
+	
+	answer = Number( num1 ) + Number( num2 );
+	
+	document.getElementById("output-1").value = answer;
+	
+}
