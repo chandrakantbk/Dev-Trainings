e6f932612c6f4209fc864266b22d355c03c546a7
chandrakantbk <chandrakantbk.cs@gmail.com>
2020-02-14 09:19:02 +0530

    Working with JS Events

--- JavaScript/Day-83-84-JSEvents/events.js
+++ JavaScript/Day-83-84-JSEvents/events.js
@@ -4,6 +4,66 @@ document.addEventListener("DOMContentLoaded", startProcessing)
 
 function startProcessing(event){
 	
+	
+	//addMyEvents();
+	
+	
+	
+}
+
+function addMyEvents(){
+	
+	//addMouseEvents()
+	
+	
+	var textBox2 = document.getElementById("num-2");
+	
+	
+	textBox2.addEventListener("paste", function(event){  
+		console.log( event );
+		
+		event.preventDefault();
+	
+		return false;
+		
+	} );
+	
+	
+	textBox2.onselect = function( event ){
+		console.log( event )
+		event.preventDefault();
+	}
+	
+	var i = 0;
+		
+	document.ondblclick = async function( event ){  
+		
+		console.log(event.target);
+		
+		console.log( event.target.tagName )
+		
+		var url = 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json';
+		
+		var externalResponse = await fetch(url);
+		
+		var data = await externalResponse.json();
+		
+		console.log( data )
+		
+		 if( event.target.tagName == "INPUT"){
+			 
+			 event.target.value = data.dataseries[  i  ].wind10m.direction;
+			 
+			 i++;
+		 }
+		
+	} 
+	
+	
+}
+
+
+function addMouseEvents() {
 	var isMouseDown = false;
 	
 	var img = document.getElementById('img-1');
@@ -54,38 +114,7 @@ function startProcessing(event){
 		
 	}
 	
-	var i = 0;
-		
-	document.ondblclick = async function( event ){  
-		
-		console.log(event.target);
-		
-		console.log( event.target.tagName )
-		
-		var url = 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json';
-		
-		var externalResponse = await fetch(url);
-		
-		var data = await externalResponse.json();
-		
-		console.log( data )
-		
-		 if( event.target.tagName == "INPUT"){
-			 
-			 event.target.value = data.dataseries[  i  ].wind10m.direction;
-			 
-			 i++;
-		 }
-		
-	} 
-	
-	
-	
-	
 }
-
-
-
 
 
 
