ec2f0be690dda7a78f289443755530c6acf9f1ce
chandrakantbk <chandrakantbk.cs@gmail.com>
2020-04-13 18:19:05 +0530

    custom media player

---
+++ jQuery/Day-110-Multimedia-demo/src/js/config.js
@@ -0,0 +1,20 @@
+const config = {
+    container: '.player-container',
+    videoElement: '.player-video',
+    playPauseBtn: '.playpause-btn',
+    progressBar: '.progress-bar',
+    progress: '.filled-area',
+    currentTimer: '.current-time',
+    durationTimer: '.duration-time',
+    advertiseContainer: '.advertise-container',
+
+    icons: {
+        'play': 'icon-play3',
+        'puase': 'icon-pause2'
+    },
+
+    adversiseTimes: [2, 6, 10],
+
+}
+
+Object.freeze(config);
--- jQuery/Day-110-Multimedia-demo/src/js/cubks_player.js
+++ jQuery/Day-110-Multimedia-demo/src/js/cubks_player.js
@@ -1,19 +1,80 @@
 /** Creating Video Player 
  * Author: CUBKS 
  */
+const player = function() {
+    let self = this;
+    self.isEventAdded = false;
+    var vid = $(config.videoElement).get(0);
+    vid.onloadeddata = function() {
+        self.addMediaEvents();
+    }
+
+    self.addMediaEvents = function() {
+        if (isEventAdded) { return false }
+        $(config.playPauseBtn).removeClass('disabled');
+
+        var uniqueTime;
+        var vid = $(config.videoElement).get(0);
+        var currentProgress = 0;
+
+        $(config.playPauseBtn).on("click", function() {
+            $(config.durationTimer).html(Math.round(vid.duration));
+
+            if (vid.paused) {
+                $(config.advertiseContainer).slideUp();
+                vid.play();
+                $(this).addClass(config.icons.puase).removeClass(config.icons.play);
+
+            } else {
+                vid.pause();
+                $(this).addClass(config.icons.play).removeClass(config.icons.puase);
+            }
+        });
+
+        $(config.videoElement).on('timeupdate', function(event) {
+            let time = Math.round(vid.currentTime);
+            let duration = Math.round(vid.duration);
+
+            /** Avoid to re-run the function on same time again, 
+             * because timeupdate fires more than once on the same second eg. 1.25 sec then again on 1.65 sec 
+             * to avoid that we are using a variable uniqueTime
+             * */
+            if (uniqueTime == time) {
+                return false;
+            }
+
+            uniqueTime = time;
+
+            console.log(time);
+
+            currentProgress = time / duration * 100;
+
+            $(config.progress).css({ 'width': currentProgress + '%' });
+
+            $(config.currentTimer).html(time);
+
+            if (config.adversiseTimes.includes(time)) {
+                vid.pause();
+                $(config.advertiseContainer).slideDown();
+            }
+
+        });
+
+        $(config.videoElement).on("ended", function() {
+            $(config.playPauseBtn).addClass(config.icons.play).removeClass(config.icons.puase);
+        })
+
+        $(config.videoElement).on("pause", function() {
+            $(config.playPauseBtn).addClass(config.icons.play).removeClass(config.icons.puase);
+        });
+
+        player.isEventAdded = true;
+
+    }
+
+}
+
 
 $(document).ready(function() {
-
-    $(".playpause-btn").on("click", function() {
-
-        var vid = $(".player-video").get(0);
-        console.log(vid)
-
-        if (vid.paused) {
-            vid.play();
-        } else {
-            vid.pause();
-        }
-
-    })
+    player();
 })
