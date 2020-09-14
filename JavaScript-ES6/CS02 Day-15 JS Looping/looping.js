243a8a28796fece2b2a716b71a300e5a08de7ce5
chandrakantbk <chandrakantbk.cs@gmail.com>
2020-09-14 22:18:28 +0530

    using for-in loop

--- JavaScript-ES6/CS02 Day-15 JS Looping/looping.js
+++ JavaScript-ES6/CS02 Day-15 JS Looping/looping.js
@@ -1,13 +1,17 @@
-// for (var num = 1; num <= 10; num++) {
-//     console.log("Welcome " + num)
-// }
+/*
+for (var num = 1; num <= 10; num++) {
+    console.log("Welcome " + num)
+}
+*/
 
 /*
 for (var num = 10; num > 0; num--) {
     console.log(num)
 }
+
 */
 
+/*
 
 var total_employess = 1000;
 
@@ -17,4 +21,43 @@ var current_emp = total_employess; //1000
 
 for (var current_emp = 1000; current_emp > 700; current_emp--) {
     console.log(current_emp);
+}
+
+*/
+
+// Day 17 For loop varients
+
+var numbers_array = [10, 20, 30, 40];
+
+/*
+    numbers_array = [];
+    
+    numbers_array.length;  // 4
+                           // 3
+
+
+    for (i = 0; i <= 3; i++) {
+        console.log(i, numbers_array[i] )
+    }
+
+*/
+
+/*
+    for (var index in numbers_array) {
+
+        //console.log(index) // string 
+
+        console.log( numbers_array[index] );
+
+        // console.log(numbers_array[Number(index) + 1])
+        // console.log("--------------------")
+    }
+
+*/
+
+// Angular 
+//  numbers_array = [];
+
+for (var val of numbers_array) {
+    console.log(val) // 10
 }
