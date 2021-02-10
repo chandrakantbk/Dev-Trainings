5a995c05a95439c5c1ef57ed8b5c6f1f9522d150
chandrakantbk <chandrakantbk.cs@gmail.com>
2021-02-10 22:20:10 +0530

    js revision event

--- B03-Angular11-Jan-21/JS-Example/basics.js
+++ B03-Angular11-Jan-21/JS-Example/basics.js
@@ -2,16 +2,15 @@
 
 //alert(name)
 /*
-a = prompt("Enter your name");
-alert(a)
-var choice = confirm("Do you want to process?");
+    a = prompt("Enter your name");
+    alert(a)
+    var choice = confirm("Do you want to process?");
 
-if (choice) {
-    alert(choice)
-}
+    if (choice) {
+        alert(choice)
+    }
 
 */
-
 
 
 let a = 50;
@@ -37,7 +36,7 @@ for (var index = 0; index < arr.length; index++) {
  */
 
 // for-of 
-/* for (let v of student) {
+/* for (let v of arr2) {
     console.log(v)
 } */
 
@@ -80,4 +79,161 @@ function check(obj2) {
     return false
 }
 
+// 9-Feb-2021 
 
+// Array methods 
+
+var student_list = [
+    { name: "Sanjay", doj: "21-02-2020" },
+    { name: "KK", doj: "21-02-2008" },
+    { name: "KK", doj: "21-02-2007" },
+]
+
+var mixed_arr = [50, false, [50, 30, 50], {}]
+
+for (let val of mixed_arr) {
+
+    if (typeof val == "boolean") {
+        // boolean
+    }
+
+    if (typeof val == "object") {
+
+        if (Array.isArray(val) == true) {
+            // array
+
+        } else {
+            // object
+        }
+
+    }
+
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+add(5, 5)
+
+var g = add;
+
+g(10, 10)
+
+var k_fun = function (a, b) { }
+
+k_fun(5, 5)
+
+// sort 
+
+// -1 
+// 0 
+// 1
+
+
+// find 
+let res = student_list.find(function (item) {
+    return item.name == "KK"
+})
+
+// filter
+
+let filter_res = student_list.filter(function (e) { return e.name == "KK" })
+
+var list = [50, 60, 82, 63, 90]
+
+// return an array of matching elements
+
+function getItems(list) {
+
+    var temp_arr = [];
+
+    for (let val of list) {
+        if (val % 5 == 0) {
+            temp_arr.push(val)
+        }
+    }
+
+    return temp_arr;
+
+}
+
+
+/* let k;
+console.log(k)
+
+k = 10; */
+
+// 10-Feb-2021 
+/*
+    JS Events
+    String Methods
+    Date
+    functions
+    Execution
+    Typescript
+*/
+
+function handleEvent(e) {
+    console.log(e);
+    e.target.classList.add("bg-color")
+    alert("I am working")
+}
+
+function handleAdd() {
+
+    var textbox1 = document.getElementById("t1");
+    var textbox2 = document.getElementsByClassName("form-control")[1];
+
+    console.log(textbox1)
+    console.log(textbox2)
+
+    var a1 = Number(textbox1.value) || 0;
+    var b1 = +textbox2.value
+
+    var z = add(a1, b1);
+
+    var res_box = document.getElementById("result");
+    res_box.value = z;
+
+    var box = document.getElementsByClassName("result-area"); //[]
+
+    box[0].innerHTML = z
+
+}
+
+// Mouse event 
+/*
+
+    click
+    dblclick
+    mouseover
+    mouseenter
+    mousedown
+    mouseup
+
+*/
+
+// keyboard 
+
+/*
+    keydown
+    keyup
+    keypress
+*/
+
+// focus
+// blur
+
+function callButtonEvent(e) {
+    document.getElementsByClassName("popup")[0].classList.toggle("hide")
+    console.log("Child button")
+    console.log(e)
+    e.stopPropagation();
+}
+
+function hidePopup() {
+    console.log("Parent Div event")
+    document.querySelector(".popup").classList.add("hide");
+
+}
