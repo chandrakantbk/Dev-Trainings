89a98bbf1ad4ddb97cc175a03af432659a0df3e5
chandrakantbk <chandrakantbk.cs@gmail.com>
2021-02-13 23:41:56 +0530

    one way binding

--- B03-Angular11-Jan-21/Portfolio/src/app/login/login.component.ts
+++ B03-Angular11-Jan-21/Portfolio/src/app/login/login.component.ts
@@ -4,14 +4,16 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-login',
   template: `
     <div class="login-form">
+      <p>{{ title  }}</p>
       <input type="text" placeholder="Username">
       <input type="password">
       <button>LOGIN</button>
   </div>`,
-
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  title = "LOGIN FORM";
 
   constructor() { }
 
